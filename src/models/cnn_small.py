6bf65bbd7ad8267e4f9a383dec959b6636f0d3bf
Serena Zhang <serenazhang@DNa80869e.SUNet>
2022-12-09 04:31:03 -0800

    tried organizing everything but crnn_with_att.ipynb and crnn.ipynb are glitching due to batch size change

---
+++ src/models/cnn_small.py
@@ -0,0 +1,80 @@
+import torch
+from torch import nn
+
+
+class CNN(torch.nn.Module):
+
+    def __init__(self, drop_p=0.2):
+        super().__init__()
+        # 4 conv blocks / flatten / linear / softmax
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=1,
+                out_channels=8,
+                kernel_size=5,
+                stride=2,
+                padding=0
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+            nn.BatchNorm2d(8),
+            nn.Dropout(p=drop_p),
+        )
+
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=8,
+                out_channels=12,
+                kernel_size=3,
+                stride=1,
+                padding=0
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+            nn.BatchNorm2d(12),
+            nn.Dropout(p=drop_p),
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=12,
+                out_channels=12,
+                kernel_size=3,
+                stride=1,
+                padding=0,
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+            nn.BatchNorm2d(12),
+            nn.Dropout(p=drop_p),
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=12,
+                out_channels=16,
+                kernel_size=3,
+                stride=1,
+                padding=0,
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+            nn.BatchNorm2d(16),
+            nn.Dropout(p=drop_p),
+        )
+        self.flatten = nn.Flatten()
+        self.linear = nn.Linear(224, 2)
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, input_data):
+        #nomralization
+        std = input_data.std()
+        input_data -= input_data.mean()
+        input_data /= std
+        
+        x = self.conv1(input_data)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.flatten(x)
+        logits = self.linear(x)
+        predictions = self.softmax(logits)
+        return predictions
