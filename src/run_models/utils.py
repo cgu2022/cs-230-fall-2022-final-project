748239a728fc6652ab9305aefd57b0a3506d8a7d
Serena Zhang <serenazhang@DNa80869e.SUNet>
2022-12-09 14:07:53 -0800

    started main.py

---
+++ src/run_models/utils.py
@@ -0,0 +1,180 @@
+import os
+import torch
+from torch import nn
+from torch.utils.data import Dataset, DataLoader
+import pandas as pd
+from tqdm import tqdm
+from torch_audiomentations import Compose, PitchShift, TimeInversion, AddBackgroundNoise, AddColoredNoise, PolarityInversion
+import torchaudio
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorboard
+from torch.utils.tensorboard import SummaryWriter
+from sklearn.metrics import classification_report, confusion_matrix, f1_score
+
+
+SAMPLE_RATE = 16000
+
+mel_spectrogram = torchaudio.transforms.MelSpectrogram(
+    sample_rate=SAMPLE_RATE,
+    n_fft=1024,
+    hop_length=512,
+    n_mels=128
+).to(device)
+
+augmentations = Compose(
+        transforms=[
+            PitchShift(
+                mode = "per_example",
+                p=0.5,
+                sample_rate=SAMPLE_RATE,
+                output_type="tensor"
+                ),
+            TimeInversion(
+                mode = "per_example",
+                p=0.5,
+                output_type="tensor"
+            ),
+            AddColoredNoise(
+                mode = "per_example",
+                p=0.5,
+                sample_rate=SAMPLE_RATE,
+                output_type="tensor"
+            ),
+            PolarityInversion(
+                mode="per_example",
+                p=0.5,
+                output_type="tensor"
+            ), 
+
+        ], output_type="tensor"
+    )
+
+def count_correct(logits, y_true):
+    y_pred = torch.argmax(logits, axis = 1)
+    return torch.sum(y_pred==y_true)
+
+def train_single_epoch(model, train_data_loader, val_data_loader, loss_fn, optimiser, device, do_augment=False):
+    total_loss_train = 0.0
+    correct_pred_train = 0.0
+    total_pred_train = 0
+
+    train_trues = []
+    train_preds = []
+
+    for x_batch, y_batch in tqdm(train_data_loader):
+        x_batch, y_batch = x_batch.to(device), y_batch.to(device)
+
+        if do_augment:
+            x_batch = augmentations(x_batch, SAMPLE_RATE)
+        
+
+        x_batch = x_batch.reshape(-1, x_batch.shape[-1])
+        x_batch = mel_spectrogram(x_batch)
+        x_batch = x_batch.reshape(x_batch.shape[0], 1, x_batch.shape[-2], x_batch.shape[-1])
+        
+        # calculate loss
+        y_pred = model(x_batch)
+        loss = loss_fn(y_pred, y_batch)
+
+        # add to list for f1 score
+        train_trues.append(y_batch.cpu())
+        train_preds.append(y_pred.cpu())
+        
+        correct_pred_train += count_correct(y_pred, y_batch)
+        total_pred_train += y_batch.shape[0]
+
+        # backpropagate error and update weights
+        optimiser.zero_grad()
+        loss.backward()
+        optimiser.step()
+        total_loss_train += loss.item()
+
+        
+        
+    print(f"Training loss: {total_loss_train}, Training accuracy : {correct_pred_train/total_pred_train}")
+    #print normalized loss
+    print(f"Training loss normalized: {total_loss_train/len(train_data_loader)}")
+    # print f1 score, precision, recall
+    # print(classification_report(torch.cat(train_trues).detach().cpu().numpy(), torch.argmax(torch.cat(train_preds), axis=1).detach().cpu().numpy()))
+    
+    total_loss_val = 0.0
+    correct_pred_val = 0.0
+    total_pred_val = 0
+
+    val_trues = []
+    val_preds = []
+
+    for x_batch, y_batch in tqdm(val_data_loader):
+        with torch.no_grad():
+            x_batch, y_batch = x_batch.to(device), y_batch.to(device)
+
+            x_batch = x_batch.reshape(-1, x_batch.shape[-1])
+            x_batch = mel_spectrogram(x_batch)
+            x_batch = x_batch.reshape(x_batch.shape[0], 1, x_batch.shape[-2], x_batch.shape[-1])
+
+            y_pred = model(x_batch)
+            loss = loss_fn(y_pred, y_batch)
+            total_loss_val += loss.item() 
+            
+            val_trues.append(y_batch.cpu())
+            val_preds.append(y_pred.cpu())
+
+        correct_pred_val += count_correct(y_pred, y_batch)
+        total_pred_val += y_batch.shape[0]
+        
+    print(f"Validataion loss: {total_loss_val}, Validation accuracy : {correct_pred_val/total_pred_val}")
+    #print normalized loss
+    print(f"Validation loss normalized: {total_loss_val/len(val_data_loader)}")
+    #print f1 score, confusion matrix and precision, recall using sklearn and true and predicted values
+    # print(classification_report(torch.cat(val_trues).detach().cpu().numpy(), torch.argmax(torch.cat(val_preds), axis=1).detach().cpu().numpy()))
+    return total_loss_train/len(train_data_loader), correct_pred_train/total_pred_train, total_loss_val/len(val_data_loader), correct_pred_val/total_pred_val
+
+
+    
+def train(model, train_data_loader, val_data_loader, loss_fn, optimiser, device, epochs, do_augment, WRITER_PATH, MODEL_FOLDER):
+    writer = SummaryWriter(WRITER_PATH)
+    for i in range(epochs):
+        print(f"Epoch {i+1}")
+        train_loss, train_acc, val_loss, val_acc = train_single_epoch(model, train_data_loader, val_data_loader, loss_fn, optimiser, device)
+        writer.add_scalar("train/accuracy", train_acc, i)
+        writer.add_scalar("train/loss", train_loss, i)
+        writer.add_scalar("validation/accuracy", val_acc, i)
+        writer.add_scalar("validation/loss", val_loss, i)
+        
+        path = os.path.join(MODEL_FOLDER, f"epoch_{i}.pth")
+        torch.save(model.state_dict(), path)
+        print(f"Saved at {path}")
+        print("---------------------------")
+    print("Finished training")
+    print("---------------------------")
+
+    
+    
+def evaluate(model, eval_data_loader, loss_fn, device):
+    print("Evaluating model")
+    total_loss = 0.0
+    correct_pred = 0.0
+    total_pred = 0
+    for x_batch, y_batch in tqdm(eval_data_loader):
+        with torch.no_grad():
+            x_batch, y_batch = x_batch.to(device), y_batch.to(device)
+
+            x_batch = x_batch.reshape(-1, x_batch.shape[-1])
+            x_batch = mel_spectrogram(x_batch)
+            x_batch = x_batch.reshape(x_batch.shape[0], 1, x_batch.shape[-2], x_batch.shape[-1])
+
+            y_pred = model(x_batch)
+            loss = loss_fn(y_pred, y_batch)
+
+            correct_pred += count_correct(y_pred, y_batch)
+            total_pred += y_batch.shape[0]
+
+            total_loss += loss.item()
+
+    print(f"Evaluation loss: {total_loss}, Evaluation accuracy : {correct_pred/total_pred}")
+    # print normalized loss
+    print(f"Evaluation loss normalized: {total_loss/len(eval_data_loader)}")
+    print("---------------------------")        
+            
+        
