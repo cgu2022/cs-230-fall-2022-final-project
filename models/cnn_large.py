ee857e1f4a229aefda38b5448e8f9360144b5aaa
Nick Tantivasadakarn <nantanick.tanti@gmail.com>
2022-12-09 19:10:26 -0800

    first draft of train wrapper

--- models/cnn_large.py
+++ models/cnn_large.py
@@ -2,74 +2,57 @@ import torch
 from torch import nn
 
 
-class CNN(torch.nn.Module):
-
+class CNN_Large(torch.nn.Module):
     def __init__(self, drop_p=0.2):
         super().__init__()
         # 4 conv blocks / flatten / linear / softmax
         self.conv1 = nn.Sequential(
             nn.Conv2d(
-                in_channels=1,
-                out_channels=8,
-                kernel_size=5,
-                stride=2,
-                padding=0
-            ),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2),
-            nn.BatchNorm2d(8),
-            nn.Dropout(p=drop_p),
-        )
-
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(
-                in_channels=8,
-                out_channels=12,
-                kernel_size=3,
-                stride=1,
-                padding=0
-            ),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2),
-            nn.BatchNorm2d(12),
-            nn.Dropout(p=drop_p),
-        )
-        self.conv3 = nn.Sequential(
-            nn.Conv2d(
-                in_channels=12,
-                out_channels=12,
-                kernel_size=3,
-                stride=1,
-                padding=0,
-            ),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2),
-            nn.BatchNorm2d(12),
-            nn.Dropout(p=drop_p),
-        )
-        self.conv4 = nn.Sequential(
-            nn.Conv2d(
-                in_channels=12,
-                out_channels=16,
-                kernel_size=3,
-                stride=1,
-                padding=0,
+                in_channels=1, out_channels=16, kernel_size=3, stride=2, padding=0
             ),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2),
             nn.BatchNorm2d(16),
             nn.Dropout(p=drop_p),
         )
+
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=16, out_channels=32, kernel_size=3, stride=2, padding=0
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=1),
+            nn.BatchNorm2d(32),
+            nn.Dropout(p=drop_p),
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=0
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=1),
+            nn.BatchNorm2d(64),
+            nn.Dropout(p=drop_p),
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=0
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=1),
+            nn.BatchNorm2d(128),
+            nn.Dropout(p=drop_p),
+        )
         self.flatten = nn.Flatten()
-        self.linear = nn.Linear(224, 2)
+        self.linear = nn.Linear(31744, 2)
         self.softmax = nn.Softmax(dim=-1)
 
     def forward(self, input_data):
-        #nomralization
-        std = input_data.std()
-        input_data -= input_data.mean()
-        input_data /= std
-        
+        # nomralization
+        # std = input_data.std()
+        # input_data -= input_data.mean()
+        # input_data /= std
+
         x = self.conv1(input_data)
         x = self.conv2(x)
         x = self.conv3(x)
